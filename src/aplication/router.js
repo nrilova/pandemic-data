c1c790637436f12353b7a850c57ee0270e8145c4
nrilova <116728539+nrilova@users.noreply.github.com>
2023-01-12 11:59:45 +0100

    first commit

---
+++ src/aplication/router.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from '../App.js'
+
+
+
+ function Router() {
+    return (
+        <BrowserRouter>
+            <Routes>
+        <Route path="/" element={<App />}></Route>
+            </Routes>
+        </BrowserRouter>
+    );
+}
+export default Router;
