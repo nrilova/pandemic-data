076bd53a20b831c042940c26040dfe3f2d5c86af
nrilova <116728539+nrilova@users.noreply.github.com>
2023-01-13 13:00:27 +0100

    create view-2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+//import Navbar from './components/landing/navbar/Navbar'
+//import Traker1 from './components/trakers/traker-1/Traker-1'
+import Section2 from './components/landing/section-2/Section-2'
 
 function App() {
   return (
     <div className="App">
-      
+    
+      <Section2/>
     </div>
   );
 }
