fce6103a5eaa8fec2179dfe85f330e2af3961687
123meena_github <habibimina5@gmail.com>
2023-01-13 11:49:47 +0100

    New commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
-
+import Navbar from '../src/components/landing/navbar/Navbar';
+import Card from './components/landing/section-3/Card';
+import CardReact from './components/landing/section-3/CardReact';
+import Section from './components/landing/section-3/Section-3';
+import Section4 from './components/landing/section-4/Section-4';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar />
+      <Card />
+      <CardReact />
+      <Section />
+      <Section4 />
+
     </div>
   );
 }
---
+++ src/components/landing/navbar/Navbar.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Button from 'react-bootstrap/Button';
+import '../../landing/navbar/navbar.css'
+
+ function Navegation() {
+  return (
+    <div>
+        <Navbar className="nav"expand="lg">
+      <Container className="navbar ">
+        <Navbar.Brand href="#home">COVIMAP</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+          <NavDropdown title="Home" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="#link">Prevention</Nav.Link>
+            <Nav.Link href="#link">Qurantine</Nav.Link>
+            <NavDropdown title="Pages" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link href="#link">About</Nav.Link>
+            <Nav.Link href="#link">Help</Nav.Link>
+            <Button ClassName="boton_tracker"variant="primary" size="lg">Tracker</Button>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    </div>
+  )
+}
+
+
+
+export default Navegation;
