911adfe335b68b9df2197046db691e2340d4290c
Mónica R <a20monicarr@gmail.com>
2023-01-22 21:32:32 +0100

    deleted files

---
+++ src/components/views/trakers/TrakerApp/TrakerApp.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './trakerApp.css';
+import Traker3 from '../traker-3/Traker-3';
+
+function TrakerApp() {
+  return (
+    <div>
+        <Traker3/>
+    </div>
+  )
+}
+
+export default TrakerApp
